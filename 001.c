e955af875a613b9b590090b632dbabded5905cdd
FunFunHaDa <kjhkjh0929@gmail.com>
2018-05-28 23:43:17 +0900

    1

---
+++ 001.c
@@ -0,0 +1,12 @@
+// 첫번째 프로그램
+
+#include<stdio.h>
+
+int main(void)
+{
+	puts(" 첫 번째 프로그램");
+	printf(" C프로그래밍은 어렵지만 재밌다. \n");
+	printf(" 코딩을 끝냈으면 Ctrl + F5키를 누르세요. \n");
+
+	return 0;
+}
---
+++ 002.c
@@ -0,0 +1,22 @@
+// putchar - 1
+
+// C언어 코딩 :: 한 문자 출력 putchar
+
+#include<stdio.h>
+
+int main(void)
+{
+	puts(" 한 문자 출력 ");
+	putchar('A');
+	putchar('B');
+	putchar('\t');
+	putchar('a');
+	putchar('b');
+	putchar('\n');
+	putchar('0');
+	putchar('1');
+	putchar('\n');
+
+	puts(" putchar함수는 한 문자 출력 전용함수");
+	return 0;
+}
