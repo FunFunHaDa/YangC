eb0d75a420e93124168b6c775fe41c09e21709fd
FunFunHaDa <kjhkjh0929@gmail.com>
2018-05-30 23:40:09 +0900

    3

---
+++ 026.c
@@ -0,0 +1,30 @@
+// 026 - 자료형의 크기 sizeof연산자
+
+#include<stdio.h>
+
+int main(void)
+{
+	puts("--------------------------");
+	puts(" 자료형의 크기");
+	puts("--------------------------");
+
+	printf(" char = %d byte \n", sizeof(char));
+	puts("--------------------------");
+
+	printf(" short = %d byte \n", sizeof(short));
+	puts("--------------------------");
+
+	printf(" int = %d byte \n", sizeof(int));
+	puts("--------------------------");
+
+	printf(" long = %d byte \n", sizeof(long));
+	puts("--------------------------");
+
+	printf(" float = %d byte \n", sizeof(double));
+	puts("--------------------------");
+
+	puts(" C언어가 제공하는 기본 자료형은 6개");
+	puts("--------------------------");
+
+	return 0;
+}
