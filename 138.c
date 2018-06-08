94a63016837960b0af42b72be6ea1ffa6d8260cf
FunFunHaDa <kjhkjh0929@gmail.com>
2018-06-08 23:53:17 +0900

    12

---
+++ 138.c
@@ -0,0 +1,3 @@
+// 138 - 우선순위표
+
+// https://msdn.microsoft.com/ko-kr/library/2bxt6kc4.aspx
---
+++ 145-2.c
@@ -0,0 +1,21 @@
+// 145 - C언어 fscanf함수 scanf함수와 차이점
+
+// 실수 입출력 입니다.
+
+#include<stdio.h>
+int main()
+{
+	float vision;
+
+	fputs(" scanf 함수 - float \n", stdout);
+
+	fprintf(stdout, " 당신의 시력은 : ");
+
+	fscanf(stdin, "%f", &vision);
+
+	fprintf(stdout, "당신의 시력은 %.1f \n", vision);
+
+	fputs("\n------------------------------------\n", stdout);
+	fputs(" %f ==> float %lf ==> double \n", stdout);
+	fputs(" -------------------------------------\n", stdout);
+}
